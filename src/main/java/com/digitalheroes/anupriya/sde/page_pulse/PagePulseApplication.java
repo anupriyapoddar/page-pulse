09acc980658494f7b591d3ba71d0163d7a240ee2
Anupriya Poddar <anupriyapoddar2003@gmail.com>
2026-07-25 18:25:38 +0530

    chore: initial spring boot setup with jsoup

---
+++ src/main/java/com/digitalheroes/anupriya/sde/page_pulse/PagePulseApplication.java
@@ -0,0 +1,13 @@
+package com.digitalheroes.anupriya.sde.page_pulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagePulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagePulseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digitalheroes/anupriya/sde/page_pulse/PagePulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalheroes.anupriya.sde.page_pulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagePulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
